22b51cee23be99e77f95b259aa8dae8f9d2735d6
Nik4End7L <cortexd95@gmail.com>
2025-10-25 10:32:06 +0600

    Postgres added

--- Rest_API_blog/settings.py
+++ Rest_API_blog/settings.py
@@ -88,10 +88,15 @@ WSGI_APPLICATION = 'Rest_API_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',         
+        'USER': 'postgres',         
+        'PASSWORD': 'QwertyHitler1488AND1943',  
+        'HOST': 'localhost',        
+        'PORT': '5432',             
     }
 }
+
 
 
 # Password validation
